0c45c1da48b44f1170547f442a770c2ca1c2ada3
Bastien <bastien@lacour75.com>
2024-04-23 14:22:00 +0200

    First commit

---
+++ app/src/main/java/fr/android/projetmobile/MainActivity.java
@@ -0,0 +1,24 @@
+package fr.android.projetmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMap(View view) throws IOException {
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        startActivity(intent);
+    }
+}
